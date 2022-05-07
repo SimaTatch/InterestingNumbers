e14218d8f4f9280de75338ee0a5173767383d0de
Sima <sima_tatchenkova@mail.ru>
2022-05-07 18:29:04 +0300

    Initial Commit

---
+++ InterestingNumbersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterestingNumbersApp
+//
+//  Created by Серафима  Татченкова  on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
